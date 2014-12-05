f76200e3e9173b3976f435c3c06cbc72ac8ab5b6
Niko Köbler <niko@n-k.de>
2014-12-05 15:02:07 +0100

    Create beer-bar.js

---
+++ beer-bar.js
@@ -0,0 +1,8 @@
+var eventBus = require("vertx/event_bus");
+
+eventBus.registerHandler("bar", function(message, replier) {
+  java.lang.System.err.println("BAR: Someone ordered " + message.amount + " beer");
+  replier({wait_time: message.amount * 1.75});
+});
+
+java.lang.System.err.println("The BAR is open!");
